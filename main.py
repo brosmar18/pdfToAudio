aebcfd6215cd2074c985b0ba233da997e73bcf65
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-26 13:06:05 -0500

    text from pdf read successfully

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from gtts import gTTS
-import os 
+import os
 import PyPDF2
 
 # Open the PDF file
 pdf_file = open('resume2023.pdf', 'rb')
 
-# PDF reader object
-pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+# Create a PDF reader object
+pdf_reader = PyPDF2.PdfReader(pdf_file)
 
+# Extract text from each page
+text = ''
+for page in pdf_reader.pages:
+    text += page.extract_text()
+
+# Close the PDF file
+pdf_file.close()
+
+# Print the extracted text
+print(text)

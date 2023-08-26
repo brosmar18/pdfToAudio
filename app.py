e73b7ca81d686f0236e76924b56f4ee94861a2f7
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-26 13:27:14 -0500

    Extract text from pdf file

--- app.py
+++ app.py
@@ -3,3 +3,18 @@ from gtts import gTTS
 import os
 import PyPDF2
 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        # Get the uploaded PDF file
+        pdf_file = request.files["pdf_file"]
+
+        # Create a PDF reader object
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+
+        # Extract text from each page
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
